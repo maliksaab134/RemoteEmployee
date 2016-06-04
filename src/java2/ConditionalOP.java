304fc1fe26ae52624f58cd889c6ec2409a9a4cd7
Malik Nadeem Awan <Malik Nadeem Awan@192.168.10.8>
2016-06-04 08:17:52 -0700

    First Commit

---
+++ src/java2/ConditionalOP.java
@@ -0,0 +1,17 @@
+package java2;
+
+public class ConditionalOP {
+
+	public static class Test {
+
+		   public static void main(String args[]){
+		      int a, b;
+		      a = 10;
+		   b= (a == 10) ? 20: 30;
+		      System.out.println( "Value of b is : " +  b );
+
+		      b = (a == 1) ? 20: 30;
+		      System.out.println( "Value of b is : " + b );
+		   }
+		}
+}
---
+++ src/java2/Inctanceof.java
@@ -0,0 +1,12 @@
+package java2;
+
+public class Inctanceof {
+
+		   public static void main(String args[]){
+		      String name = "James";
+		      boolean result = name instanceof String;  
+		      System.out.println( result );
+		   }
+		}
+
+
